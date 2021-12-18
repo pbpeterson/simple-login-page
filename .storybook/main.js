799b6127fe4aad565250d8b4e62037ff22fe9c89
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-18 19:45:11 -0300

    Add storybook and its initial structure

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
+  ],
+  "framework": "@storybook/react"
+}
