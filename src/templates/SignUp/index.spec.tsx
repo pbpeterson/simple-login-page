5b36900dbc7a7bb92dca085b2ef3fb206101a36b
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-11 15:19:43 -0300

    Add SignUp Template

---
+++ src/templates/SignUp/index.spec.tsx
@@ -0,0 +1,8 @@
+import SignUp from '.'
+import { render } from '@testing-library/react'
+
+describe('SignUp', () => {
+  it('should render SignUp', () => {
+    render(<SignUp />)
+  })
+})
---
+++ src/templates/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import FormSignUp from '../../components/FormSignUp'
+import * as S from './styles'
+
+const SignUp = () => (
+  <S.Wrapper>
+    <S.LeftSide>
+      <S.Title>Prensa</S.Title>
+    </S.LeftSide>
+    <S.RightSide>
+      <FormSignUp />
+    </S.RightSide>
+  </S.Wrapper>
+)
+
+export default SignUp
